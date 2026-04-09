7f1ee5ebd3d0b0ba73863dfa37bc666bedcd3519
Amir Zarrinkafsh <3339418+nightah@users.noreply.github.com>
2026-04-09 15:30:43 +1000

    ci(buildkite): support merge queues (#152)
    * ci(buildkite): support merge queues
    
    This change adds support for GitHub merge queues.
    
    * ci(buildkite): add merge queue no-op
    
    * ci(buildkite): simplify skips directly with merge queue detection
    
    * ci: run github workflows in merge queues

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,6 +5,13 @@ REGISTRIES="docker.io ghcr.io"
 REPOSITORY="authelia/oidc-tester-app"
 TAGS=""
 
+CI_MERGE_QUEUE="false"
+
+if [[ ${BUILDKITE_BRANCH} =~ ^gh-readonly-queue/.* ]]; then
+  CI_MERGE_QUEUE="true"
+  buildkite-agent annotate --style "info" --context "ctx-info" < .buildkite/annotations/merge-queue
+fi
+
 if [[ "${BUILDKITE_BRANCH}" =~ ^renovate/ ]]; then
   TAGS="renovate"
 elif [[ "${BUILDKITE_BRANCH}" != "master" ]] && [[ ! "${BUILDKITE_BRANCH}" =~ .*:.* ]]; then
@@ -25,7 +32,11 @@ steps:
   - label: ":docker: Build and Deploy"
     command: "docker build ${BUILDTAGS::-1} --label org.opencontainers.image.source=https://github.com/${REPOSITORY} --platform linux/amd64,linux/arm64 --provenance mode=max,reproducible=true --sbom true --builder buildx --progress plain --pull --push ."
 EOF
-if [[ "${BUILDKITE_BRANCH}" == "master" ]]; then
+if [[ "${CI_MERGE_QUEUE}" == "true" ]]; then
+cat << EOF
+    skip: "Skipping CI/CD steps for merge queue build."
+EOF
+elif [[ "${BUILDKITE_BRANCH}" == "master" ]]; then
 cat << EOF
     concurrency: 1
     concurrency_group: "oidc-tester-app-deployments"
@@ -42,5 +53,13 @@ cat << EOF
       - "build-docker-linux"
     agents:
       upload: "fast"
+EOF
+if [[ "${CI_MERGE_QUEUE}" == "true" ]]; then
+cat << EOF
+    skip: "Skipping CI/CD steps for merge queue build."
+EOF
+else
+cat << EOF
     if: build.branch == "master"
 EOF
+fi
